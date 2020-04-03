7198cbe7f67c58c2586df69b392da367aec23802
Kafune <kachung97@hotmail.com>
2020-04-03 16:45:11 +0200

    gameengine testing

--- src/main/java/nl/han/ica/oopd/bubbletrouble/BubbleTrouble.java
+++ src/main/java/nl/han/ica/oopd/bubbletrouble/BubbleTrouble.java
@@ -12,8 +12,14 @@ public class BubbleTrouble extends GameEngine {
 	private Bubble bubble;
 	private Player player;
 	
+	public static String MEDIA_URL = "src/main/resources/bubble-trouble/";
+	
 	public static void main(String[] args) {
-		PApplet.main(new String[] { "nl.han.ica.oopd.bubbletrouble.BubbleTrouble" });
+//		PApplet.main(new String[] { "nl.han.ica.oopd.bubbletrouble.BubbleTrouble" });
+		String[] processingArgs = {"nl.han.ica.oopd.bubbletrouble.BubbleTrouble"};
+		BubbleTrouble bubbleTrouble = new BubbleTrouble();
+		
+		PApplet.runSketch(processingArgs, bubbleTrouble);
 
 	}
 
@@ -24,27 +30,37 @@ public class BubbleTrouble extends GameEngine {
         
         createDashBoard(worldWidth, 100);
         
+        createObjects();
+        
         viewPortScreen(worldWidth, worldHeight);
 	}
 
 	@Override
 	public void update() {
-		
+		// Hierin komen de methodes die over tijd gebeuren. bubbels die uit elkaar splitten bijv.
 	}
 	
 	public void createDashBoard(int dashboardWidth, int dashboardHeight) {
 		Dashboard dashboard = new Dashboard(0, 620, dashboardWidth, dashboardHeight);
+		dashboardText = new TextObject("Testtext", 14);
+		refreshDashboardText();
+		dashboard.addGameObject(dashboardText);
+		addDashboard(dashboard);
 	}
 	
 	public void viewPortScreen(int screenWidth, int screenHeight) {
 		View view = new View(screenWidth, screenHeight);
-		view.setBackground(loadImage("src/main/resources/bubble-trouble/startscreen.jpg"));
+		view.setBackground(loadImage(MEDIA_URL + "startscreen.jpg"));
 		setView(view);
 		size(screenWidth, screenHeight);
 	}
 	
 	private void refreshDashboardText() {
 		dashboardText.setText("TODO"); //Score refresher.
+	}
+	
+	private void createObjects() {
+		
 	}
 	
 }
--- src/main/java/nl/han/ica/oopd/bubbletrouble/Player.java
+++ src/main/java/nl/han/ica/oopd/bubbletrouble/Player.java
@@ -1,5 +1,20 @@
 package nl.han.ica.oopd.bubbletrouble;
 
-public class Player {
+import nl.han.ica.oopg.objects.AnimatedSpriteObject;
+import nl.han.ica.oopg.objects.Sprite;
 
+public class Player extends AnimatedSpriteObject{
+	private BubbleTrouble bubbletrouble;
+	
+	public Player(BubbleTrouble bubbletrouble) {
+		super(new Sprite("src/main/resources/bubble-trouble/player.png"), 2);
+		this.bubbletrouble = bubbletrouble;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
